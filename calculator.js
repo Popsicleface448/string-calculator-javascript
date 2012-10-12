e413143903f5641fa0348f0f12000d329019e3fd
Damon <damaneice@gmail.com>
2012-10-11 21:11:54 -0500

    refactored string calculator

--- calculator.js
+++ calculator.js
@@ -1,11 +1,34 @@
 var add = function(string) {
   var digits = function() {
-    var arr = [],
-        tokens = string.split(/[^\d|^-]/);
+
+  	var findCustomDelimiter = function(string){
+    	return string.substring(2, findNewline(string));  
+  	};
+
+  	var hasCustomDelimiter = function(string){
+    	return string.indexOf('//') > -1;
+  	};
+
+		var findNewline = function(string) {
+    	return string.indexOf('\n');
+		};
+  	var extractString = function(string){ 
+  		return hasCustomDelimiter(string) ? string.slice(findNewline(string) + 1) : string;
+  	};
+    
+  	var createDelimiter = function(string){
+    	var delimiter = ',\n';
+   		if(hasCustomDelimiter(string)){
+				delimiter = findCustomDelimiter(string) + delimiter; 
+    	}
+	  	return '[' + delimiter + ']';
+  	};
+ 
+		var arr = [], delimiter = new RegExp(createDelimiter(string)),
+        tokens = extractString(string).split(delimiter);
     for (var i = 0; i<tokens.length; i++) {
       arr.push(Number(tokens[i]));
     }
-
     return arr;
   }();
   
