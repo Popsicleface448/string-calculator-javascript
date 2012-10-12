165a62185ce64dbb7738b47ab880a87facc67546
Damon <damaneice@gmail.com>
2012-10-11 21:39:03 -0500

    cleaned up formatting of code

--- calculator.js
+++ calculator.js
@@ -2,34 +2,34 @@ var add = function(string) {
   var digits = function() {
 
   	var findCustomDelimiter = function(){
-    	return string.substring(2, findNewline());  
+    		return string.substring(2, findNewline());  
   	};
 
   	var hasCustomDelimiter = function(){
-    	return string.indexOf('//') > -1;
+    		return string.indexOf('//') > -1;
   	};
 
-		var findNewline = function() {
-    	return string.indexOf('\n');
-		};
+	var findNewline = function() {
+    		return string.indexOf('\n');
+	};
   	var extractString = function(){ 
   		return hasCustomDelimiter() ? string.slice(findNewline() + 1) : string;
   	};
     
   	var createDelimiter = function(){
-    	var delimiter = ',\n';
+    		var delimiter = ',\n';
    		if(hasCustomDelimiter()){
-				delimiter = findCustomDelimiter() + delimiter; 
-    	}
-	  	return '[' + delimiter + ']';
+			delimiter = findCustomDelimiter() + delimiter; 
+    		}		
+		return '[' + delimiter + ']';
   	};
  
-		var arr = [], delimiter = new RegExp(createDelimiter()),
+	var arr = [], delimiter = new RegExp(createDelimiter()),
         tokens = extractString().split(delimiter);
-    for (var i = 0; i<tokens.length; i++) {
-      arr.push(Number(tokens[i]));
-    }
-    return arr;
+    	for (var i = 0; i<tokens.length; i++) {
+      		arr.push(Number(tokens[i]));
+    	}	
+	return arr;
   }();
   
   var sum = 0;
